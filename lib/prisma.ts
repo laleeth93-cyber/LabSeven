f0602f3102acad23af810101683edc9d15f1175b
laleeth93-cyber <laleeth93@gmail.com>
2026-04-17 01:09:13 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,2 +1,10 @@
-// 🚨 GHOST BYPASS: Completely empty of imports to satisfy Vercel
-export const prisma = {} as any;
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// We create a new PrismaClient without the { datasources: ... } object
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
